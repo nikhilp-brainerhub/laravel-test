f50724a43d2da611102768dd827409ebce8705d4
Nikhil Parmar <nikhilparmar27@gmail.com>
2022-06-30 18:22:13 +0530

    Task#4 - Completed the getMenuItems function

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -94,6 +94,8 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        // throw new \Exception('implement in coding task 3');
+        $menus = MenuItem::whereNull('parent_id')->with('children')->get()->toArray();
+        return response()->json($menus);
     }
 }
